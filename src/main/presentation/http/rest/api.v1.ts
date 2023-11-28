6c5efeacafb5a9f250f16ffd9ad8501bdc26e077
Fernanda Alves <f31304702@gmail.com>
2023-11-28 20:56:21 -0300

    aula dia 28/11/23 Terça-Feira

--- src/main/presentation/http/rest/api.v1.ts
+++ src/main/presentation/http/rest/api.v1.ts
@@ -1,7 +1,17 @@
 
 import express, { Router } from 'express';
+import { categoriaRouter } from '@modules/catalogo/presentation/http/rest/categoria.routes';
 
 const apiv1Router: Router = express.Router();
+
+
+
+
+
+apiv1Router.use(
+    '/categorias',
+    categoriaRouter
+);
 
 apiv1Router.use(
     '/categorias',
